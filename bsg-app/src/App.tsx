e7b1e4d496fbeb5d28bd676cf120b0ba2c120ca7
xXrederXx <morgenthalerthierry@gmail.com>
2025-04-18 20:02:27 +0200

    testNode

--- bsg-app/src/App.tsx
+++ bsg-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import {
   ReactFlow,
   MiniMap,
@@ -12,17 +12,24 @@ import {
 } from '@xyflow/react';
  
 import '@xyflow/react/dist/style.css';
+import TextUpdaterNode from './Nodes/testNode';
  
 const initialNodes: Node[] = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '1', type: "textUpdater", position: { x: 0, y: 0 }, data: { label: '1' } },
   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
 ];
 const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
+
+const nodeTypes = {
+  textUpdater: TextUpdaterNode
+};
+
+
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
+  const nodeTypes = useMemo(() => ({ textUpdater: TextUpdaterNode }), []);
+
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges],
@@ -37,6 +44,7 @@ export default function App() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         colorMode={"dark"}
+        nodeTypes={nodeTypes}
       >
         <Controls />
         <MiniMap />
