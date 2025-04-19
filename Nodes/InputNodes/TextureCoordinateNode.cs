7f9dcebca94c3736d316100497579806699e6d62
xXrederXx <morgenthalerthierry@gmail.com>
2025-04-19 20:37:53 +0200

    TexCoord. To Node

---
+++ Nodes/InputNodes/TextureCoordinateNode.cs
@@ -0,0 +1,92 @@
+using System.Text.Json;
+using blenderShaderGraph.Types;
+using blenderShaderGraph.Util;
+
+public enum TextureCoordinateType
+{
+    Object,
+}
+
+public record TextureCoordinateProps
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public TextureCoordinateType Mode { get; set; } = TextureCoordinateType.Object;
+}
+
+public class TextureCoordinateNode : Node<TextureCoordinateProps, MyColor[,]>
+{
+    public TextureCoordinateNode()
+        : base() { }
+
+    public TextureCoordinateNode(string Id, JsonElement element)
+        : base(Id, element) { }
+
+    protected override TextureCoordinateProps SafeProps(TextureCoordinateProps props)
+    {
+        return props;
+    }
+
+    protected override MyColor[,] ExecuteInternal(TextureCoordinateProps props)
+    {
+        MyColor[,] res = new MyColor[props.Width, props.Height];
+        return props.Mode switch
+        {
+            TextureCoordinateType.Object => Object(res),
+            _ => res,
+        };
+    }
+
+    protected override TextureCoordinateProps ConvertJSONToProps(Dictionary<string, object> contex)
+    {
+        JsonElement p = element.GetProperty("params");
+
+        TextureCoordinateType type = p.GetString("mode", "") switch
+        {
+            "object" => TextureCoordinateType.Object,
+            _ => TextureCoordinateType.Object,
+        };
+        return new()
+        {
+            Width = p.GetInt("width", 1024),
+            Height = p.GetInt("height", 1024),
+            Mode = type,
+        };
+    }
+
+    protected override void AddDataToContext(MyColor[,] data, Dictionary<string, object> contex)
+    {
+        contex[Id] = data;
+    }
+
+    // NODE SPESIFIC
+    private static MyColor[,] Object(MyColor[,] img)
+    {
+        int width = img.GetLength(0);
+        int height = img.GetLength(1);
+        int halfWidth = width / 2;
+        int halfHeight = height / 2;
+
+        MyColor[,] newColors = new MyColor[width, height];
+
+        Parallel.For(
+            0,
+            width,
+            (x) =>
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    float t = MyMath.Map(x, halfWidth, width, 1, 0);
+                    byte r = MyMath.ClampByte(MyMath.Lerp(1, 0, t) * 255);
+
+                    t = MyMath.Map(y, 0, halfHeight, 0, 1);
+                    byte g = MyMath.ClampByte(MyMath.Lerp(1, 0, t) * 255);
+
+                    newColors[x, y] = new MyColor(r, g, 0);
+                }
+            }
+        );
+
+        return img;
+    }
+}
