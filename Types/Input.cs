6961a8f3f3c2c54bd2a561cbc2ad373c04fca3c9
xXrederXx <morgenthalerthierry@gmail.com>
2025-04-19 17:19:16 +0200

    pragma statements

--- Types/Input.cs
+++ Types/Input.cs
@@ -2,7 +2,13 @@ public class Input<T>
 {
     public T this[int x, int y]
     {
-        get { return useArray ? Values[x, y] : Value; }
+        // Not able to be null
+#pragma warning disable CS8603 // Possible null reference return.
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        get => useArray ? Values[x, y] : Value;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+#pragma warning restore CS8603 // Possible null reference return.
+
         set { Console.Write("WARNING: Cant set value of Input<T>"); }
     }
 
