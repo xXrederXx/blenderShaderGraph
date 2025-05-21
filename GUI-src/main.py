a6f270675921ea6f09cefc8cb70e53e3d45fe605
xXrederXx <morgenthalerthierry@gmail.com>
2025-05-21 14:23:39 +0200

    fix style bug

--- GUI-src/main.py
+++ GUI-src/main.py
@@ -1,29 +1,36 @@
 """Avoid lint"""
 
-import customtkinter as ctk
-from NodeApp import NodeApp
-from globals.my_logger import MyLogger
-from globals.paths import TMP_PATH, PERSISTANT_PATH, LOG_PATH, AUTO_SAVE_PATH
-
 if __name__ == "__main__":
-    #Check paths created
+    # Check paths created
+    from globals.paths import TMP_PATH, PERSISTANT_PATH, LOG_PATH, AUTO_SAVE_PATH
+
     TMP_PATH.mkdir(parents=True, exist_ok=True)
     PERSISTANT_PATH.mkdir(parents=True, exist_ok=True)
     LOG_PATH.mkdir(parents=True, exist_ok=True)
     AUTO_SAVE_PATH.mkdir(parents=True, exist_ok=True)
-    
-    #Setting up logging
+
+    # Setting up logging
+    from globals.my_logger import MyLogger
+
     log = MyLogger(10)
     log.set_up_logger()
     log.set_logger_output_console(True)
 
     log.info("Starting Node App")
 
-    #Setting up ctk
+    # Setting up ctk
+    import customtkinter as ctk
+
     ctk.set_appearance_mode("Dark")
     ctk.set_default_color_theme("dark-blue")
 
-    #Running app
+    from globals.style import StyleManager, get_dark_theme, get_light_theme
+
+    StyleManager.use_theme(get_dark_theme)
+
+    # Running app
+    from NodeApp import NodeApp
+
     root = NodeApp()
     root.mainloop()
 
